1a03a9c100e2efde76aa2937cd8bac69a305e896
Lojacopsen <68278515+Lojacopsen@users.noreply.github.com>
2021-03-30 11:13:23 +0200

    Update random commands.py

--- random commands.py
+++ random commands.py
@@ -5,11 +5,17 @@ from random import choice
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.') #prefisso del bot
-
 @client.event
 async def on_ready():
     change_status.start()
-    print('ready leroro')
+    print('ready leroro2!')
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('Leroro'):
+        await message.channel.send('PREGATEMI SCHIAAAVI')
     
 client.run('your token') #the token -_-
