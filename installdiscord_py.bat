7b5ebec8d9497e334d2e8e868dae1442d7feb4da
Lojacopsen <68278515+Lojacopsen@users.noreply.github.com>
2021-04-12 15:39:44 +0200

    Update installdiscord_py.bat

--- installdiscord_py.bat
+++ installdiscord_py.bat
@@ -1,6 +1,5 @@
 @echo off
 start powershell
 pause before installation you need to have python 3.6 or later, continue?
-npm
-npm Install discord.py
+start py -3 -m pip install -U discord.py
 rem REQUIRES PYTHON 3.6 OR LATER FOR THE INSTALLATION and NPM
