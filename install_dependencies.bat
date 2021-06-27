44c4b5f79b3d6bf5b25dc6a9c76670d88b577d23
Lojacopsen <68278515+Lojacops@users.noreply.github.com>
2021-06-27 14:11:45 +0200

    Delete install_dependencies.bat

--- install_dependencies.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start
-echo before installation you need to have python 3.6 or later, continue?
-pause
-start py -3 -m pip install -U discord.py
-rem REQUIRES PYTHON 3.6 OR LATER FOR THE CORRECT INSTALLATION
